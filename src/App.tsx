5f4712056beb7dd013885ce0857f35d666662306
Robert Mack <robertmack123123@gmail.com>
2024-09-29 09:37:39 -0500

    Added buttons and routing to summer, winter, and custom mixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import '@aws-amplify/ui-react/styles.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "@aws-amplify/ui-react/styles.css";
 import Login from "./Login";
-import PreLoginNav from './Navigation/PreLoginNav.jsx';
-import WeatherDisplay from './Weather/weather';
-
-
+import PreLoginNav from "./Navigation/PreLoginNav.jsx";
+import WeatherDisplay from "./Weather/weather";
+import WinterMix from "./Mixes/WinterMix.jsx";
+import SummerMix from "./Mixes/SummerMix.jsx";
+import CustomMix from "./Mixes/CustomMix.jsx";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/wintermix" element={<WinterMix />} />
+        <Route path="/summermix" element={<SummerMix />} />
+        <Route path="/custommix" element={<CustomMix />} />
       </Routes>
     </Router>
   );
@@ -19,9 +23,10 @@ function App() {
 
 function HomeScreen() {
   return (
-    <><div>
-      <PreLoginNav />
-    </div>
+    <>
+      <div>
+        <PreLoginNav />
+      </div>
       <div>
         <h1>Welcome to Simplifry!</h1>
       </div>
@@ -29,7 +34,8 @@ function HomeScreen() {
         <h3>Check the weather before you go!</h3>
         <WeatherDisplay />
       </div>
-    </>);
+    </>
+  );
 }
 
 export default App;
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,44 +1,58 @@
 import { useEffect, useState } from "react";
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
-import PreLoginNav from './Navigation/PreLoginNav';
-import SummerMix from "./Mixes/SummerMix";
-
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import PreLoginNav from "./Navigation/PreLoginNav";
+// import SummerMix from "./Mixes/SummerMix";
+// import WinterMix from "./Mixes/WinterMix";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    
-    const [showSummerMix, setShowSummerMix] = useState(false);
-    useEffect(() => {
-        
-    }, []);   
+  //   const [showSummerMix, setShowSummerMix] = useState(false);
+  //   const [showWinterMix, setShowWinterMix] = useState(false);
+  const navigate = useNavigate();
 
-    
-    function summermixes() {
-        setShowSummerMix((prev) => !prev)
-    }
-    
+  function goToWinterMix() {
+    navigate("/wintermix");
+  }
 
-    return (
-        <>
-        <div>
+  function goToSummerMix() {
+    navigate("/summermix");
+  }
+
+  function goToCustomMix() {
+    navigate("/custommix");
+  }
+
+  //   function summermixes() {
+  //     setShowSummerMix((prev) => !prev);
+  //   }
+  //   function winterMixes() {
+  //     setShowWinterMix((prev) => !prev);
+  //   }
+
+  return (
+    <>
+      <div>
         <PreLoginNav />
-        </div>
-            
-        <Authenticator>
-            {({ signOut, user }) => (
-                <main>
-                        <h1>{user?.signInDetails?.loginId}'s Console</h1>
-                        <h3>Click on the mix you are using</h3>
-                        
-                        <button onClick={summermixes}>Summer Mix</button>
-                        {showSummerMix && <SummerMix />}                    
-                    
-                    <button onClick={signOut}>Sign out</button>
-                </main>
+      </div>
 
-            )}
-            </Authenticator>
-        </>
-    );
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>{user?.signInDetails?.loginId}'s Console</h1>
+            <h3>Click on the mix you are using</h3>
+
+            <button onClick={goToSummerMix}>Summer Mix</button>
+
+            <button onClick={goToWinterMix}>Winter Mix</button>
+
+            <button onClick={goToCustomMix}>Custom Mix</button>
+
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </>
+  );
 }
 export default Login;
